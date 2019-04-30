5eaa1c4e90875795362bdaec210d886525b4b84d
Kent Ketter <kent.ketter1@gmail.com>
2019-04-30 09:26:13 -0700

    server file

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+//enviroment variable defined in .env as 3000
+const PORT = process.env.PORT || 3000;
+
+console.log(process.env.TEST);
+
+app.use(express.static('./public'));
+
+app.get('/isitworking', (request, response) => {
+  response.send('yes');
+});
+
+app.get('/TEST', (request, response) => {
+  try {
+    let testData = require('./data/darksky.JSON');
+    response.send(testData);
+  } catch (error) {
+    console.log('there was en error');
+    response.status(500).send('sorry, error');
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
