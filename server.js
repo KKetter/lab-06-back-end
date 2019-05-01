f74f472f7ca644b6b3073905e00da3a809ed1bb3
Kent Ketter <kent.ketter1@gmail.com>
2019-04-30 17:50:51 -0700

    working error

--- server.js
+++ server.js
@@ -31,12 +31,17 @@ app.get('/TEST', (request, response) => {
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
 app.get('/location', (request, response) => {
-  let jsonInfo = require('./data/geo.json');
-  let someLocation = new GeoObject(jsonInfo.results[0].address_components[0].long_name, jsonInfo.results[0].formatted_address, jsonInfo.results[0].geometry.location.lat, jsonInfo.results[0].geometry.location.lng);
-  response.send(someLocation);
+  try {
+    let jsonInfo = require('./data/geo.json');
+    let someLocation = new GeoObject(jsonInfo.results[0].address_components[0].long_name, jsonInfo.results[0].formatted_address, jsonInfo.results[0].geometry.location.lat, jsonInfo.results[0].geometry.location.lng);
+    response.send(someLocation);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('Status: 500. So sorry, something went wrong');
+  }
 });
 
-function GeoObject(query, address, latitude, longitude){
+function GeoObject(query, address, latitude, longitude) {
   this.formatted_query = query;
   this.formatted_address = address;
   this.latitude = latitude;
@@ -44,17 +49,22 @@ function GeoObject(query, address, latitude, longitude){
 }
 
 app.get('/weather', (request, response) => {
-  let jsonInfo = require('./data/darksky.json');
-  let weatherDates = [];
-  for (let index = 0; index < jsonInfo.daily.data.length; index++) {
-    let tempObj = new WeatherObject(jsonInfo.daily.data[index].summary, jsonInfo.daily.data[index].time);
-    weatherDates.push(tempObj);
-  }
+  try {
+    let jsonInfo = require('./data/darksky.json');
+    let weatherDates = [];
+    for (let index = 0; index < jsonInfo.daily.data.length; index++) {
+      let tempObj = new WeatherObject(jsonInfo.daily.data[index].summary, jsonInfo.daily.data[index].time);
+      weatherDates.push(tempObj);
+    }
 
-  response.send(weatherDates);
+    response.send(weatherDates);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('Status: 500. So sorry, something went wrong');
+  }
 });
 
-function WeatherObject(forecast, time){
+function WeatherObject(forecast, time) {
   this.forecast = forecast;
   this.time = new Date(time * 1000).toDateString();
 }
